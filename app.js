c69b3468c993445c58ae5bd5c4b9b28009d53821
Vanessa Ferrer <vanessalimaoi729@gmail.com>
2024-10-28 05:16:46 +0000

    Pending changes exported from your codespace

---
+++ app.js
@@ -0,0 +1,16 @@
+//variables para crear el dominio
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.es', '.eu'];
+
+
+function generaDomains(){
+    for(let i= 0; i < pronoun.length; i++){
+
+    }for(let)
+}
+
+console.log(${pronoun});
+
